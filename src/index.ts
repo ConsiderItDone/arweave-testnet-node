379dd55ff782fc257bffce4a179cd02b3a6c2870
BERLl4 <berlinova1314@gmail.com>
2024-05-03 12:13:25 +0300

    Added env for app port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import ArLocal from "arlocal";
 import {topUpWalletBalance} from "./script";
+import * as dotenv from 'dotenv';
+import * as process from "node:process";
 
-const arLocal = new ArLocal();
+dotenv.config();
+
+const arLocal = new ArLocal(Number(process.env.APP_PORT) || 1984);
 (async () => {
     await arLocal.start();
 
